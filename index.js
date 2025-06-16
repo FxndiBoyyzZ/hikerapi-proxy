5e755ed1eb9127b2b41bfa0a26c3537574d5da03
FxndiBoyyzZ <fluxo.tegon@gmail.com>
2025-06-16 16:02:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,26 +9,29 @@ const PORT = process.env.PORT || 3000;
 const HIKER_TOKEN = process.env.HIKER_TOKEN || 'dau2pb0u4m2ceea61bfu5mxwhg06i51r';
 
 app.get('/api/profile', async (req, res) => {
-  const id = req.query.id;
-  if (!id) {
-    return res.status(400).json({ error: 'ID is required (e.g., /api/profile?id=123456)' });
+  const username = req.query.username;
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required (e.g., /api/profile?username=neymarjr)' });
   }
 
   try {
-    const response = await axios.get(`https://api.hikerapi.com/v2/user/by/id`, {
+    const response = await axios.get(`https://api.hikerapi.com/v1/user/by/username`, {
       headers: {
         'x-access-key': HIKER_TOKEN,
         'accept': 'application/json',
       },
       params: {
-        id,
+        username,
       },
     });
 
     res.json(response.data);
   } catch (error) {
     console.error(error.response?.data || error.message);
-    res.status(500).json({ error: 'Erro ao buscar perfil', details: error.response?.data || error.message });
+    res.status(500).json({
+      error: 'Erro ao buscar perfil',
+      details: error.response?.data || error.message,
+    });
   }
 });
 
